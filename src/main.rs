b783f8e9d9ba937fe9589871ec786e158dc82981
whseeeew <whseeeew@gmail.com>
2022-11-10 21:17:49 +0900

    feat: A new Feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// Yewモジュールから全てを利用
+// 一般的なimport -> Module yew::prelude
+use yew::prelude::*
+
+// 関数コンポーネントのアトリビュートを付与
+#[function_component(App)]
+fn app() -> html {
+    // htmlマクロを発火
+    html! {
+        <h1>{"Hello World"}</h1>
+    }
+}
+
+// エントリーポイント
 fn main() {
-    println!("Hello, world!");
+    // Appコンポーネントをbodyにマウント
+    yew::start_app::<App>();
 }
